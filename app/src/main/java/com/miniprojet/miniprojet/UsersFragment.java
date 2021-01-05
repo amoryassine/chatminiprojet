e4c1b1fdafe7cb4064e9806fc87c040d5aa85b9c
amoryassine <amor9999amor@gmail.com>
2021-01-05 18:00:14 +0100

    list users

--- app/src/main/java/com/miniprojet/miniprojet/UsersFragment.java
+++ app/src/main/java/com/miniprojet/miniprojet/UsersFragment.java
@@ -1,10 +1,20 @@
 package com.miniprojet.miniprojet;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -12,6 +22,10 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class UsersFragment extends Fragment {
+    RecyclerView recyclerView ;
+    AdapterUsers adapterUsers;
+    List<ModelUser> userList;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -57,6 +71,56 @@ public class UsersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false);
+        View view= inflater.inflate(R.layout.fragment_users, container, false);
+        // init recycler view
+        recyclerView =view.findViewById(R.id.users_recyclerView);
+        // set it's properties
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // init user list
+        userList =new ArrayList<>();
+
+        // getAll users
+        getAllUsers();
+
+
+        return view;
+    }
+
+    private void getAllUsers() {
+        // get current user
+        final FirebaseUser fUser= FirebaseAuth.getInstance().getCurrentUser();
+
+        // get path of database named "users" containing users info
+
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Users");
+
+        // get all data from path
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot dataSnapshot) {
+                userList.clear();
+                for (DataSnapshot ds :dataSnapshot.getChildren()){
+                    ModelUser modelUser= ds.getValue(ModelUser.class);
+
+                    // get all users exept currently signed in user
+                    if (!modelUser.getUid().equals(fUser.getUid())){
+                        userList.add(modelUser);
+                    }
+                    // adapter
+                    adapterUsers =new AdapterUsers(getActivity(),userList);
+
+                    // set adapter to recycler view
+                    recyclerView.setAdapter(adapterUsers);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
     }
 }
